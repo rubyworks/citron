5ac150b36c01bb42ffbe451fed2e06919dd9a453
7rans <transfire@gmail.com>
2011-07-28 22:10:38 -0400

    Have example demonstrate all status types. [doc]

--- try/case_example.rb
+++ try/case_example.rb
@@ -1,11 +1,30 @@
-Case "Show them how to Beat It" do
+TestCase "Show them how to Beat It" do
 
-  test "Show them how to Funcky" do
-    "Funcky".assert == "Funcky"
+  # will fail
+  test "show them how to funky" do
+    "funky".assert != "funky"
   end
 
-  test "Show them what's right." do
+  # will pass
+  test "show them what's right" do
     "right".assert == "right"
+  end
+
+  # will error
+  test "no one wants to be defeated" do
+    raise SyntaxError
+  end
+
+  # pending
+  test "better do what you can" do
+    raise NotImplementedError
+  end
+
+  # omit
+  test "just beat it" do
+    e = NotImplementedError.new
+    e.set_assertion(true)
+    raise e
   end
 
 end
