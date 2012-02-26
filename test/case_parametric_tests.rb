0be048c90bf86810d61995ab4ae921275398364c
7rans <transfire@gmail.com>
2012-02-25 22:42:42 -0500

    Update setup test. [test]

---
+++ test/case_parametric_tests.rb
@@ -0,0 +1,10 @@
+testcase "parametric tests" do
+
+  test "example parametric test" do |x|
+    x.assert.even?
+  end
+
+  ok 2
+  ok 4
+
+end
--- test/case_setup.rb
+++ test/case_setup.rb
@@ -1,6 +1,5 @@
-test_case "Example of using setup in a testcase" do
-
-  setup do
+testcase "Example of using setup in a testcase" do
+  setup "the number one" do
     @x = 1
   end
 
@@ -8,16 +7,20 @@ test_case "Example of using setup in a testcase" do
     @x.assert == 1
   end
 
-  setup "has a topic" do
-    @x = 10
+  context "sub-case inherits parent setup" do
+    test "has setup" do
+      @x.assert == 1
+    end
   end
 
-  test "has setup with a topic" do
-    @x.assert == 10
-  end
+  context "sub-case with setup override parent setup" do
+    setup "has setup" do
+      @y = 10
+    end
 
-  test "alos has setup with a topic" do
-    @x.assert! == 5
+    test "has setup" do
+      @x.assert == nil
+      @y.assert == 10
+    end
   end
-
 end
